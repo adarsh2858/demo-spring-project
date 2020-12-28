d9a114e9b788c08992b26f53ee67f4779994c0fe
adarsh <adarsh2858@gmail.com>
2020-12-28 22:42:18 +0530

    bugfix and launch a working hello world spring app

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	@RequestMapping("/course")
-	public void courses() {
-		System.out.println("Returning courses");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
